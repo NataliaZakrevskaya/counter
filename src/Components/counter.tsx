45e14208a59d99cfec4dbe482c5ec2c3a0c61776
Natalia <zakrevskaya.97@inbox.ru>
2022-03-02 18:22:57 +0300

    Deleted Localstorage buttons, added useEffects

--- src/Components/counter.tsx
+++ src/Components/counter.tsx
@@ -1,9 +1,23 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import s from "./counter.module.css"
 
 export const Counter = () => {
 
     const [count, setCount] = useState<number>(0)
+
+    useEffect( () => {
+        let valueAsString = localStorage.getItem('counterValue')
+        if (valueAsString) {
+            let valueAsNumber = JSON.parse(valueAsString)
+            setCount(valueAsNumber)
+        }
+    }, [])
+
+    useEffect( () => {
+        localStorage.setItem('counterValue', JSON.stringify(count))
+    }, [count] )
+
+
 
     const incrementCount = () => {
         setCount(count + 1)
@@ -14,30 +28,16 @@ export const Counter = () => {
     const resetCount = () => {
         setCount(0)
     }
-    const setToLocalstorage = () => {
-        localStorage.setItem('counterValue', JSON.stringify(count))
-    }
-    const getFromLocalstorage = () => {
-        let valueAsString = localStorage.getItem('counterValue')
-        if (valueAsString) {
-            let valueAsNumber = JSON.parse(valueAsString)
-            setCount(valueAsNumber)
-        }
-    }
 
     return (
         <div className={s.counterContainer}>
             <div className={s.window}>{count}</div>
             <div className={s.incDecBtnContainer}>
-                <button onClick={incrementCount} disabled={count === 5 ? true : false}>inc</button>
+                <button onClick={incrementCount} disabled={count === 100 ? true : false}>inc</button>
                 <button onClick={decrementCount} disabled={count === 0 ? true : false}>dec</button>
             </div>
             <div className={s.resetBtnContainer}>
                 <button onClick={resetCount} disabled={count === 0 ? true : false}>Reset</button>
-            </div>
-            <div className={s.localStorageBtnContainer}>
-                <button onClick={setToLocalstorage}>Set to Localstorage</button>
-                <button onClick={getFromLocalstorage}>Get from Localstorage</button>
             </div>
         </div>
     );
